d0ccbc6ae611c7f1dabbc572317e04f3fc8cb44f
Mauricio Szajman <mausza@bu.edu>
2025-04-18 21:32:59 -0400

    another fix for mp-5

--- mp-5/app/[alias]/route.ts
+++ mp-5/app/[alias]/route.ts
@@ -3,23 +3,16 @@ import getCollection, { LINKS_COLLECTION } from "@/db";
 
 export async function GET(
     request: NextRequest,
-    { params }: { params: { alias: string } }
+    { params }: { params: Promise<{ alias: string }> }
 ) {
-    const { alias } = params;
+    const { alias } = await params;
 
     const linksCollection = await getCollection(LINKS_COLLECTION);
-
-
     const doc = await linksCollection.findOne({ shortenedURL: alias });
 
     if (!doc) {
-        // If not found, return a 404 JSON response
-        return NextResponse.json(
-            { error: "Alias not found" },
-            { status: 404 }
-        );
+        return NextResponse.json({ error: "Alias not found" }, { status: 404 });
     }
 
-    // Redirect the client to the original URL
     return NextResponse.redirect(doc.originalURL, 302);
 }
